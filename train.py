bf3f902724a12d5b30feb3ca17e7021d169ddc13
Carlos <carlosdomhleba@gmail.com>
2024-04-30 13:48:12 +0300

    Change default iter count to 7000

--- train.py
+++ train.py
@@ -205,9 +205,11 @@ if __name__ == "__main__":
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default = None)
+    parser.add_argument("--iterations", nargs="+", type=int, default=7000)
     args = parser.parse_args(sys.argv[1:])
     args.save_iterations.append(args.iterations)
     
+    print("iterations: ", args.iterations)
     print("Optimizing " + args.model_path)
 
     # Initialize system state (RNG)
